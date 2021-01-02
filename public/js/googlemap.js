6c2c0168f69ec72f7e2bdb1825414d3272669a94
panos21kyr <panos21kyr@gmail.com>
2021-01-02 23:25:58 +0200

    infobox

--- public/js/googlemap.js
+++ public/js/googlemap.js
@@ -16,15 +16,38 @@ function initMap(json) {
         zoom: 15,
         center: mylatlng,
     });
+
+    const iconBase = 'http://localhost/project%20kokkoras/myFuel/public/js/logos/';
+    var contentString = '<div>o kwstas ton pairnei</div>';
+
     for(var i = 0; i < json.length; i++){
-        new google.maps.Marker({
-            position: { lat: parseFloat(json[i].gasStationLat), lng: parseFloat(json[i].gasStationLong) },
-            map,
-            title: "prathrio" + i,
-        });
+      const marker = new google.maps.Marker({
+          position: { lat: parseFloat(json[i].gasStationLat), lng: parseFloat(json[i].gasStationLong) },
+          map,
+          title: json[i].fuelCompNormalName,
+          icon: `${iconBase}${json[i].fuelCompNormalName}.png`,
+      });
+      
+      var infoWindow = new google.maps.Marker({
+        contet: contentString
+      });
+      
+      marker.addEventListener('click', () => {
+        infoWindow.open(map, marker);
+      });
     }
+    
+    // google.maps.event.addListener(markers,'click', (function(marker,contentString,infowindow){ 
+    //   return function() {
+    //       infowindow.setContent(contentString);
+    //       infowindow.open(map,marker);
+    //   };
+    // })(marker,contentString,infowindow)); 
 }
 
+
+
+//radio buttons
 $(document).ready(function() {
     $("input[type=radio]").change(function() {
       $("select").removeClass('myVisible myHide');
@@ -57,3 +80,4 @@ document.querySelector(".litra").addEventListener("click", () => {
   const litra = "lt";
   a.textContent = litra;
 });
+
